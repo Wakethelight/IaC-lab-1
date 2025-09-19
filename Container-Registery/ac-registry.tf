2c1e09a2b1de0e154574df011f56d8d5790afebe
wake <chris.degner@gmail.com>
2025-09-18 20:52:45 -0400

    updated container lab for web dashboard

--- Container-Registery/ac-registry.tf
+++ Container-Registery/ac-registry.tf
@@ -1,10 +1,10 @@
 #create container registry
 resource "azurerm_container_registry" "main" {
-  name                     = "acr${var.application_name}${var.environment_name}"
+  name                     = "wakeacr${var.application_name}${var.environment_name}"
   location                 = azurerm_resource_group.main.location
   resource_group_name      = azurerm_resource_group.main.name
   sku                      = "Basic"
-  admin_enabled            = false
+  #admin_enabled            = false
 
   tags = {
     environment = var.environment_name
